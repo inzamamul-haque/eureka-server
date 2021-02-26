7f43a57871668c30a640c5478e28378093df9d4e
Rifat <rifat@softbyteit.com>
2021-02-26 21:52:01 +0600

    init project

---
+++ src/test/java/com/inzamamul/discovery/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inzamamul.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
